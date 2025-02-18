f6a7cbdea67b5de1f3caa1e521ea71a30c7e41fa
HD <hlainghtun2018@gmail.com>
2025-02-18 15:32:12 +0630

    role CURD Finished

--- src/main/java/org/erd/roleoptions/impls/Rolesdb.java
+++ src/main/java/org/erd/roleoptions/impls/Rolesdb.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-
+@Transactional
 @Repository
 public class Rolesdb implements DataAccessObject<Roles> {
 
