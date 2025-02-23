561ae91c668a7a7f74abfa6293408ffb4992cf1f
Hlaing Htun <hlainghtun2018@gmail.com>
2025-02-23 19:46:30 +0630

    chart of Account Settingup

--- src/main/java/org/erd/chartofaccountoptions/model/ChartOfAccounts.java
+++ src/main/java/org/erd/chartofaccountoptions/model/ChartOfAccounts.java
@@ -1,4 +1,36 @@
 package org.erd.chartofaccountoptions.model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor
+@Entity
+@Table(name = "chart_of_accounts")
+@Component
 public class ChartOfAccounts {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int account_id ;
+
+   @NotBlank(message = "အကောင့်နာမည်လိုအပ်ပါသည်။")
+   @Column(name = "account_name")
+   private String  account_name;
+
+   @NotBlank(message = "အကောင့် အမျိုးအစား အထူးလိုအပ်ပါသည်")
+   @Column(name = "account_type")
+   private String  account_type ;
+
+   @Column(name = "is_active")
+   private byte is_active ;
+
+
+
+
 }
