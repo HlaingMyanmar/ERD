73cf99becf1786d8233a494239b8bb44aa00480e
HD <hlainghtun2018@gmail.com>
2025-02-23 19:49:40 +0630

    userdashboard view

--- src/main/java/org/erd/useroptions/controllers/UserDashboardControllers.java
+++ src/main/java/org/erd/useroptions/controllers/UserDashboardControllers.java
@@ -1,4 +1,108 @@
 package org.erd.useroptions.controllers;
 
-public class UserDashboardControllers {
+import jakarta.validation.Validator;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.erd.useroptions.models.Users;
+import org.erd.useroptions.services.UserServices;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+@Controller
+public class UserDashboardControllers implements Initializable {
+
+    @FXML
+    private TableColumn<Users, Date> DateCol;
+
+    @FXML
+    private Button addbtn;
+
+    @FXML
+    private Button editbtn;
+
+    @FXML
+    private TableColumn<Users, Integer> idCol;
+
+    @FXML
+    private TableColumn<Users, String> nameCol;
+
+    @FXML
+    private TableColumn<Users, String> passCol;
+
+    @FXML
+    private Button removebtn;
+
+    @FXML
+    private Button setrolebtn;
+
+    @FXML
+    private TableColumn<Users, String> statusCol;
+
+    @FXML
+    private TableView<Users> userdbtable;
+
+
+    private final UserServices userServices;
+
+    private final Validator validator;
+
+    private List<Users> usersList;
+
+
+    public UserDashboardControllers(UserServices userServices, Validator validator) {
+        this.userServices = userServices;
+        this.validator = validator;
+        usersList = new ArrayList<>();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        getLodRowData();
+
+        tableInill();
+
+
+    }
+
+    private void tableInill() {
+
+
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("user_name"));
+        passCol.setCellValueFactory(new PropertyValueFactory<>("password"));
+        DateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("activation"));
+    }
+
+    private void getLodRowData() {
+
+
+
+        ObservableList<Users> userList = FXCollections.observableArrayList(userServices.getAllData().stream()
+                .map(users -> {
+
+                    String status = (users.getIs_active()==1)?"active":"inactive";
+                    return new Users(users.getUser_id(),users.getUser_name(),users.getPassword(),users.getCreated_at(),status);
+                })
+                .collect(Collectors.toList()));
+
+
+        userdbtable.setItems(userList);
+
+
+
+    }
 }
--- src/main/java/org/erd/useroptions/models/Users.java
+++ src/main/java/org/erd/useroptions/models/Users.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.security.Timestamp;
+
 @Entity
 @Component
 @Getter@Setter
@@ -28,8 +30,13 @@ public class Users {
     @Column(name="password",length = 50)
     private String password;
 
+    @Column(name="created_at")
+    private Timestamp created_at;
+
     @Column(name="is_active",length = 1)
     private byte is_active;
+
+    private String activation;
 
 
     public Users(String user_name, String password, byte is_active) {
@@ -41,5 +48,21 @@ public class Users {
     public Users(String user_name, String password) {
         this.user_name = user_name;
         this.password = password;
+    }
+
+
+    public Users(int user_id, String user_name, String password, String activation) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.password = password;
+        this.activation = activation;
+    }
+
+    public Users(int user_id, String user_name, String password, Timestamp created_at, String activation) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.password = password;
+        this.created_at = created_at;
+        this.activation = activation;
     }
 }
