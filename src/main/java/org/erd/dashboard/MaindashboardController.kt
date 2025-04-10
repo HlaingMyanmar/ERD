a03479b7a0542229fdc153a9cf1aa50bf3e0d9f3
Hlaing Htun <hlainghtun2018@gmail.com>
2025-04-11 00:12:43 +0630

    dashboard access

--- src/main/java/org/erd/dashboard/MaindashboardController.kt
+++ src/main/java/org/erd/dashboard/MaindashboardController.kt
@@ -1,8 +1,15 @@
 package org.erd.dashboard
 
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
+import javafx.scene.Scene
+import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.stage.Modality
+import javafx.stage.Stage
+import org.erd.App
+import org.erd.App.context
 import org.erd.loginoptions.LoginFormController
 import org.springframework.stereotype.Controller
 import java.net.URL
@@ -10,10 +17,15 @@ import java.util.*
 
 
 @Controller
-class MaindashboardController : Initializable {
+class MaindashboardController() : Initializable {
 
     @FXML
     private lateinit var userlb: Label
+
+    @FXML
+    private lateinit var capitalbtn :Button
+
+
 
 
 
@@ -21,7 +33,30 @@ class MaindashboardController : Initializable {
 
         userlb.text = LoginFormController.userName
 
+
+        capitalbtn.setOnMouseClicked {
+
+            val stage = Stage()
+            val mainStage: Stage = capitalbtn.scene.window as Stage
+            val fxmlLoader = FXMLLoader(App::class.java.getResource("/views/capitalview/injectionsview.fxml"))
+            fxmlLoader.setControllerFactory { context.getBean(it) }
+            val scene = Scene(fxmlLoader.load())
+            stage.title = "CapitalView"
+            stage.initOwner(mainStage)
+            stage.initModality(Modality.APPLICATION_MODAL)
+            stage.scene = scene
+            stage.show()
+
+
+        }
+
+
+
+
+
     }
+
+
 
 
 }
